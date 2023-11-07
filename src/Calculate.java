16121e8ef12dcdac18f4b253c9e66a8624d8570d
l2ilai <yoasakura13@gmail.com>
2023-11-08 00:16:51 +0400

    tenth commit

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 public class Calculate {
     private int year;
-
+    private final int MIN_INPUT_YEAR = 2002;
+    private final int MAX_INPUT_YEAR = 2021;
     public void setValueYear() throws YearException {
+
 
         //Принимём данных с клавиатуры и преобразорвание в целое число
         try {
             String scanner = new Scanner(System.in).nextLine();
             year = Integer.parseInt(scanner);
-            if (!(year >= 2002 & year <= 2021)) {
+            if (!(year >= MIN_INPUT_YEAR & year <= MAX_INPUT_YEAR)) {
                 throw new YearException("Year should be from 2002 to 2021 inclusive");
             }
         } catch (NumberFormatException e) {
@@ -18,52 +20,54 @@ public class Calculate {
     }
 
     //Вычесление массива долей дохода за все года
-    private double[] getArrayRevenueShare(double[] moex_rate) {
-        double[] revenue_share = new double[moex_rate.length - 1];
-        for (int i = 0; i < moex_rate.length - 1; i++) {
-            revenue_share[i] = (moex_rate[i + 1] - moex_rate[i]) / moex_rate[i];
+    private double[] getArrayRevenueShare() {
+        double[] revenue_share = new double[Constants.MOEX_RATE.length - 1];
+        for (int i = 0; i < Constants.MOEX_RATE.length - 1; i++) {
+            revenue_share[i] = (Constants.MOEX_RATE[i + 1] - Constants.MOEX_RATE[i]) / Constants.MOEX_RATE[i];
         }
         return revenue_share;
     }
 
     //Вычесление кол-ва изымаемых средств в первом году
     private double calcWithdrawalCapitalStartingYear(double currentWithdrawalPercentage, double capitalCurrentYear) {
-        return (currentWithdrawalPercentage / 100) * capitalCurrentYear;
+        double currentWithdrawalFractions = currentWithdrawalPercentage / 100;    // перевод из проценты в доли
+        return currentWithdrawalFractions * capitalCurrentYear;
     }
 
     //Вычесление дохода с биржы за год
-    private double calcIncomeCapital(double remainingCapital, int currentYear) {
-        double[] moex_rate = Constants.MOEX_RATE;                   //индекс биржи
-        double[] revenue_share = getArrayRevenueShare(moex_rate);   //доли дохода с биржи
-        return remainingCapital * (1.0 + revenue_share[(currentYear % 2000) - 2]);
+    private double calcIncomeCapital(double remainingCapital, int indexArrayInputYear) {
+        double[] revenue_share = getArrayRevenueShare();                    //доли дохода с биржи за год
+        return remainingCapital * revenue_share[indexArrayInputYear] + remainingCapital;
     }
 
     //Вычесление изымания в следующем году с учётом инфляции
-    private double calcWithdrawalCapitalCurrentYear(double withdrawalCapitalCurrentYear, int currentYear) {
-        double[] inflation_rate = Constants.INFLATION_RATE;         //проценты инфляции
-        return withdrawalCapitalCurrentYear * (inflation_rate[(currentYear % 2000)
-                - 2] / 100) + withdrawalCapitalCurrentYear;
+    private double calcWithdrawalCapitalCurrentYear(double withdrawalCapitalCurrentYear, int indexArrayInputYear) {
+        double inflation_fractions = Constants.INFLATION_RATE[indexArrayInputYear] / 100; // перевод из проценты в доли
+        return withdrawalCapitalCurrentYear * inflation_fractions + withdrawalCapitalCurrentYear;
     }
 
     //Поиск минимального кипатала при текущем  проценте изъятия
     private boolean isMinCapitalCurrentYear(double currentWithdrawalPercentage) {
-        double capitalCurrentYear;                      //капитал текуцего года
+        double capitalCurrentYear;                       //капитал текуцего года
         double remainingCapital;                           //отстаток текучего года
-        double withdrawalCapitalNextYear;              //изымания в следующем году с учётом инфляции
+        double withdrawalCapitalNextYear;                //изымания в следующем году с учётом инфляции
+        int indexArrayInputYear = year - MIN_INPUT_YEAR;  // вычесление первого индекса массива для текущего года
 
         capitalCurrentYear = 1000.0;
         //изымание в начале первого года
         withdrawalCapitalNextYear = calcWithdrawalCapitalStartingYear(currentWithdrawalPercentage, capitalCurrentYear);
-        for (int currentYear = year; currentYear <= 2021; currentYear++) {
+        for (int currentYear = year; currentYear <= MAX_INPUT_YEAR; currentYear++) {
 
             //остаток текучего года
             remainingCapital = capitalCurrentYear - withdrawalCapitalNextYear;
 
             //доход за текущий год
-            capitalCurrentYear = calcIncomeCapital(remainingCapital, currentYear);
+            capitalCurrentYear = calcIncomeCapital(remainingCapital, indexArrayInputYear);
 
             //изымания в следующем году с учётом инфляции
-            withdrawalCapitalNextYear = calcWithdrawalCapitalCurrentYear(withdrawalCapitalNextYear, currentYear);
+            withdrawalCapitalNextYear = calcWithdrawalCapitalCurrentYear(withdrawalCapitalNextYear, indexArrayInputYear);
+
+            indexArrayInputYear += 1;
         }
         return capitalCurrentYear < 0;
     }
@@ -72,7 +76,7 @@ public class Calculate {
     protected double calcMaxWithdrawalPercentage() {
         double maxWithdrawalPercentage = 100;              //максимальный процент изъятия
 
-        if (year == 2021) {
+        if (year == MAX_INPUT_YEAR) {
             return maxWithdrawalPercentage;
         }
         for (double currentWithdrawalPercentage = 0.5; maxWithdrawalPercentage > currentWithdrawalPercentage; currentWithdrawalPercentage += 0.5) {
